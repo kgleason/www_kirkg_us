c2853e82b0bebc52f7843d86302a67e681e43999
Kirk Gleason <kgleason@gmail.com>
2022-09-05 19:45:19 -0400

    Make fields not null

--- blog/models.py
+++ blog/models.py
@@ -35,9 +35,9 @@ class Post(models.Model):
 class Media(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50, null=False, blank=False, unique=True)
-    file = models.FileField(upload_to='files/')
-    image = models.ImageField(upload_to='images/')
-    posts = models.ForeignKey(Post, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='files/', null=True, blank=True)
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    posts = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title

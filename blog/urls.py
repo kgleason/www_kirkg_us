07bbde672aa4eadfbf31b07b84546a8dc3f9883d
Kirk Gleason <kgleason@gmail.com>
2022-09-06 20:03:47 -0400

    Still working on images.

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,7 @@
 from . import views
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
